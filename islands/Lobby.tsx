4b1875d93efc7ee1655f89e8ad28aa7454e65f75
connosssss <connorgriffin2424@gmail.com>
2025-12-18 12:43:31 -0800

    Lobby rework

--- islands/Lobby.tsx
+++ islands/Lobby.tsx
@@ -11,45 +11,54 @@ interface LobbyProps {
 
 export default function Lobby({ userName, setUserName, code, setCode, onJoin, onCreate }: LobbyProps) {
     return (
-        <div class="h-full w-full flex flex-col items-center gap-12">
-            <h1 class="text-4xl mt-8 font-semibold">___</h1>
+        <div class="h-3/4 w-[40%] flex flex-col gap-12 bg-slate-800 p-10 text-white 
+        rounded-xl shadow-lg justify-center font-mono">
 
-            <div class="w-10/12 bg-slate-200 flex flex-col gap-6 text-center">
-                <h2 class="text-3xl">Username</h2>
+            {/* 
+            <h1 class="text-4xl mt-8 font-semibold w-full text-start">___</h1>
+             */}
+            
+
+            <div class="w-full flex flex-col gap-6 text-start">
+                <h2 class="text-4xl font-bold">Username</h2>
 
                 <input
                     type="text"
                     value={userName}
                     onInput={(e) => setUserName(e.currentTarget.value)}
-                    class="h-[3rem] rounded-2xl p-5 text-center"
+                    class="h-[3rem] p-5 text-center rounded-md"
                 />
-            </div>
+            </div> 
 
 
-            <div class="w-10/12 bg-slate-200 flex flex-col gap-6 text-center">
-                <h2 class="text-3xl">Lobby Code</h2>
+            <div class="w-full flex flex-col gap-6 text-start">
+                <h2 class="text-4xl font-bold">Lobby Code</h2>
                 <input
                     type="text"
                     value={code}
                     onInput={(e) => setCode(e.currentTarget.value)}
-                    class="h-[3rem] rounded-2xl p-5 text-center"/>
+                    class="h-[3rem] p-5 text-center rounded-md"/>
 
  
  
  
-                <button
-                    class="w-full h-[3rem] bg-emerald-300 rounded-2xl font-semibold"
-                    onClick={onJoin}
-                >
-                    Join Lobby
-                </button>
+                <div class="w-full flex flex-row gap-5">
+                    <button
+                        class="w-full h-[3rem] bg-slate-500 hover:bg-slate-600 shadow-md transition-colors duration-200 rounded-md font-semibold"
+                        onClick={onJoin}
+                    >
+                        Join Lobby
+                    </button>
                 
-                <button
-                    class="w-full h-[3rem] bg-emerald-300 rounded-2xl font-semibold"
-                    onClick={onCreate}
-                >
-                    Create Lobby
-            </button>            
+                    <button
+                        class="w-full h-[3rem] bg-slate-500 hover:bg-slate-600 shadow-md transition-colors duration-200 rounded-md font-semibold"
+                        onClick={onCreate}
+                    >
+                        Create Lobby
+                    </button>   
+                    
+                    
+                    </div>         
            </div>
         </div>
     );
